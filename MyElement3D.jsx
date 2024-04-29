3f0e30261244c90f28b6ad25b6ef9bb8b4ede2a1
Taehoony <164149952+taehoony123@users.noreply.github.com>
2024-04-29 15:56:56 +0900

    Add files via upload

---
+++ MyElement3D.jsx
@@ -0,0 +1,23 @@
+import { Mesh } from "three"
+import { useFrame } from "@react-three/fiber"
+import { useRef } from "react"
+
+function MyElement3D(){
+    const refMesh = useRef()
+    useFrame((state,delta)=> {
+        refMesh.current.rotation.y += delta
+    })
+
+    return(
+        <>
+            <directionalLight position={1,1,1}/>
+
+            <mesh ref = {refMesh} rotation-y={45*Math.PI/180}>
+                <boxGeometry />
+                <meshStandardMaterial color= "#e67e22" />
+            </mesh>
+        </>
+    )
+}
+
+export default MyElement3D
